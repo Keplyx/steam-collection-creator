a95402e3d76bac7584a26d172303f909254509ea
Keplyx <keplyx@gmail.com>
2018-05-06 23:38:34 +0200

    Added linux build file

--- steam-collection-creator.py
+++ steam-collection-creator.py
@@ -144,6 +144,7 @@ class HelpDialog(QDialog):
     def open_link(self, url):
         QDesktopServices.openUrl(QUrl(url))
 
+
 if __name__ == '__main__':
     with(open("data.json")) as f:
         data = json.load(f, object_pairs_hook=OrderedDict)  # preserve order
